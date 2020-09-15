ab0e7eaba014e3c2039742636a3676c16ba8c115
shehryarkh <skhan8525@gmail.com>
2020-09-15 18:02:52 -0400

    db

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ mongoose.connect(mongoURI, {
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection errors:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', () => {
   console.log(`Connected to database: ${mongoURI}`);
