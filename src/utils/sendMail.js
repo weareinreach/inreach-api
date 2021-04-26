a40eec7e0ca652ff61aa5c82627df5a511a9e5d9
FJKhan <fjgenus@gmail.com>
2021-04-26 12:55:47 -0400

    adds functionality to email user informing them a list was shared with them

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,8 +1,9 @@
-var API_KEY = 'key-414b065823760eabf0b4eb07ba6f7c52';
-var DOMAIN = 'email.asylumconnectcatalog.org';
+var API_KEY = process.env.MAILGUN_API_KEY || Cypress.env('MAILGUN_API_KEY');
+var DOMAIN = process.env.MAILGUN_DOMAIN || Cypress.env('MAILGUN_DOMAIN');
 var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
 import {User} from '../mongoose';
+import {handleBadRequest, handleErr} from './index';
 
 export const generatePasswordResetMail = async (req, res) => {
 	try {
@@ -42,5 +43,44 @@ export const generatePasswordResetMail = async (req, res) => {
 		await sendPasswordReset();
 	} catch (error) {
 		res.status(500).send(error);
+	}
+};
+
+const genereateEmailContent = (shareUrl, resourceType) => {
+	return ` <p>Hello, Bonjour, Hola!</p>
+	<p>This notification is on behalf of AsylumConnect to let you know that someone has shared a ${resourceType} with you. You can view the ${resourceType} 
+	<a href=${shareUrl} target="_blank">here</a>.</p>
+	<p>Thank you, Merci, Gracias!</p>
+	<p>The AsylumConnect Team</p>`;
+};
+
+export const shareUserList = (
+	email = null,
+	shareType = null,
+	shareUrl = null,
+	list = null,
+	res = null
+) => {
+	try {
+		if (!email || !shareType || !shareUrl || !list || !res) {
+			return handleBadRequest();
+		}
+		const resourceType =
+			shareType === 'collection' ? 'list of resources' : 'resource';
+		const html = genereateEmailContent(shareUrl, resourceType);
+		const data = {
+			from: 'AsylumConnect Support <catalog@asylumconnect.org>',
+			to: email,
+			subject: `Someone just shared ${resourceType} with you!`,
+			html: html
+		};
+		return mailgun.messages().send(data, (error, body) => {
+			if (error || !body || !body?.id || !body?.message) {
+				handleErr(error, res);
+			}
+			res.json({updated: true, list, sent: true});
+		});
+	} catch (error) {
+		return error;
 	}
 };
