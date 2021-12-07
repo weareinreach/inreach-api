7629f6dd367b2bbf54668308fb0062d20cfcc98e
FJKhan <fjgenus@gmail.com>
2021-12-07 11:52:47 -0500

    let's see if this works

--- src/mongoose.js
+++ src/mongoose.js
@@ -86,7 +86,7 @@ const OrganizationSchema = new Schema(
 				state_ES: String,
 				unit: String,
 				zip_code: String,
-				geolocation: {type: String, coordinates: [Decimal128]}
+				geolocation: {type: {type: String}, coordinates: [Decimal128]}
 			}
 		],
 		notes_log: [
