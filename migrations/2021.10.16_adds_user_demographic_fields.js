6b3fc728768847d5107ef1b1f2b7608d40f8201a
Alfred <devops.moreira@gmail.com>
2022-01-01 18:44:09 -0700

    hotfix pathing for mongoose

--- migrations/2021.10.16_adds_user_demographic_fields.js
+++ migrations/2021.10.16_adds_user_demographic_fields.js
@@ -24,7 +24,7 @@ require('dotenv').config({path: '.env'});
 // Import DB Connection
 require('../src/db');
 
-var mongoose = require('../../src/mongoose');
+var mongoose = require('../src/mongoose');
 
 if (process.env.MIGRATION) {
 	runMigrationScript();
