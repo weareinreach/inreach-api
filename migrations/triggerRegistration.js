f27d01cc675428ee1f560571a735170e1727a36f
Alfred <devops.moreira@gmail.com>
2022-06-14 00:53:46 -0600

    hotfix migration registration

--- migrations/triggerRegistration.js
+++ migrations/triggerRegistration.js
@@ -5,6 +5,9 @@ require('regenerator-runtime/runtime');
 require('dotenv').config({
 	path: '.env'
 });
+const MIGRATION_STAGE = 'migration_stage';
+const MIGRATION_PROD = 'migration_prod';
+var BODY;
 
 const axios = require('axios');
 const URL =
@@ -16,13 +19,30 @@ const HEADERS = {
 	}
 };
 
-const BODY = {
-	branch: process.env.BRANCH,
-	parameters: {
-		register_migration: true,
-		pipeline_id: process.env.PIPELINE_ID,
-		run_branch_pipeline: false,
-		files: process.env.FILES
-	}
-};
+switch (process.env.TYPE) {
+	case MIGRATION_PROD:
+		BODY = {
+			branch: process.env.BRANCH,
+			parameters: {
+				register_migration: true,
+				pipeline_id: process.env.PIPELINE_ID,
+				run_branch_pipeline: false,
+				files: process.env.FILES,
+				date_pattern: process.env.DATE_PATTERN
+			}
+		};
+		break;
+	case MIGRATION_STAGE:
+		BODY = {
+			branch: process.env.BRANCH,
+			parameters: {
+				register_migration: true,
+				pipeline_id: process.env.PIPELINE_ID,
+				run_branch_pipeline: false,
+				files: process.env.FILES
+			}
+		};
+		break;
+}
+
 axios.post(URL, BODY, HEADERS);
