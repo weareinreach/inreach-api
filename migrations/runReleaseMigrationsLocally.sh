42259f722b7c237c610392d050a8c7bf2a620a4b
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-10-10 18:21:15 -0700

    redate

--- migrations/runReleaseMigrationsLocally.sh
+++ migrations/runReleaseMigrationsLocally.sh
@@ -8,7 +8,7 @@
 
 #Use the correct date pattern all the files with a release
 #The pattern should follow yyyy.mm.dd{.|_}
-export DATE_PATTERN=2022.11.01
+export DATE_PATTERN=2022.10.11
 
 #Check if which directory script was run from
 if [[ $PWD == *inrech-api ]]; then
